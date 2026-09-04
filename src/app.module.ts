d0d3fae95012e122e41ac6c0ad5ed43857c2c075
Suleiman Masaya <masayasuleiman04@mail.com>
2026-09-04 22:52:16 +0100

    Subscription model created

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { PrismaModule } from './prisma/prisma.module';
 import { PharmacyModule } from './pharmacy/pharmacy.module';
 import { AuthModule } from './auth/auth.module';
 import { InventoryModule } from './inventory/inventory.module';
+import { SubscriptionModule } from './subscription/subscription.module';
 
 @Module({
-  imports: [PrismaModule, PharmacyModule, AuthModule, InventoryModule],
+  imports: [PrismaModule, PharmacyModule, AuthModule, InventoryModule, SubscriptionModule],
   controllers: [AppController],
   providers: [AppService],
 })
